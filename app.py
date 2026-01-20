0a704a0b77dd972e26b8b1a633130f9dd3ea5dd4
Agent46uar17 <135534537+Agent46uar17@users.noreply.github.com>
2026-01-21 01:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+import requests
 
-app = Flask(__name__)
+@app.route("/stats")
+def stats():
+    limits = requests.get(SERVER_URL, headers=HEADERS).json()
+    res = requests.get(RESOURCES_URL, headers=HEADERS).json()
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+    mem_limit = limits["attributes"]["limits"]["memory"]
+    disk_limit = limits["attributes"]["limits"]["disk"]
 
-if __name__ == "__main__":
-    app.run()
+    r = res["attributes"]["resources"]
+
+    return jsonify({
+        "state": res["attributes"]["current_state"].upper(),
+        "cpu": r["cpu_absolute"],
+        "ram": r["memory_bytes"] / 1024 / 1024,
+        "disk": r["disk_bytes"] / 1024 / 1024,
+        "mem_limit": mem_limit,
+        "disk_limit": disk_limit
+    })
